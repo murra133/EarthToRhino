81d9752e43e1b5e2352a05614ac456140d04bffa
[Nico Azel] <nazel@thorntontomasetti.com>
2024-10-26 15:47:12 -0400

    Create Earth Anchor Component and Examples
    This creates a earth anchor point component and some utility functions. an example GH file is included

--- EarthToRhino/Utilities.cs
+++ EarthToRhino/Utilities.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace EarthToRhino
@@ -10,5 +11,97 @@ namespace EarthToRhino
     {
         public static string CATEGORY_NAME = "Earth To Rhino";
         public static string SUBCATEGORY_NAME = "Tiles";
+
+        public static double DMStoDDLon(string dms)
+        {
+            ///Regex pattern for verifying DMS Longitude is with -180 to 180
+            var lonDMSPattern = @"[ ,]*(-?(180[ :°d]*00[ :\'\'m]*00(\.0+)?|(1[0-7][0-9]|0[0-9][0-9]|[0-9][0-9])[ :°d]*[0-5][0-9][ :\'\'m]*[0-5][0-9](\.\d+)?)[ :\?\""s]*(E|e|W|w)?)";
+
+            ///Regex pattern DD Longitude is with -180 to 180
+            var lonDDPattern = @"^(\+|-)?(?:180(?:(?:\.0{1,20})?)|(?:[0-9]|[1-9][0-9]|1[0-7][0-9])(?:(?:\.[0-9]{1,20})?))$";
+
+            ///Get rid of any white spaces
+            dms = dms.Trim();
+
+            double coordinate = Double.NaN;
+
+            ///Test if the Lon is DD format and return as double if valid
+            if (Regex.Match(dms, lonDDPattern).Success)
+            {
+                return Double.Parse(dms);
+            }
+
+            ///Else test if the Lon is DMS format and convert to double DD and return if valid
+            else if (Regex.Match(dms, lonDMSPattern).Success && Regex.Match(dms, lonDMSPattern).Value.Length == dms.Length)
+            {
+                bool sw = dms.ToLower().EndsWith("w");
+                int f = sw ? -1 : 1;
+                var bits = Regex.Matches(dms, @"[\d.]+", RegexOptions.IgnoreCase);
+                coordinate = 0;
+                double result;
+                for (int i = 0; i < bits.Count; i++)
+                {
+                    if (Double.TryParse(bits[i].ToString(), out result))
+                    {
+                        coordinate += result / f;
+                        f *= 60;
+                    }
+                }
+
+                return coordinate;
+            }
+
+            ///If DD or DMS format is invalid, return NaN
+            else
+            {
+                return coordinate;
+            }
+        }
+
+        public static double DMStoDDLat(string dms)
+        {
+            ///Regex pattern for verifying DMS Latitude is with -90 to 90
+            var latDMSPattern = @"(-?(90[ :°d]*00[ :\'\'m]*00(\.0+)?|[0-8][0-9][ :°d]*[0-5][0-9][ :\'\'m]*[0-5][0-9](\.\d+)?)[ :\?\""s]*(N|n|S|s)?)";
+
+            ///Regex pattern DD Latitude is with -90 to 90
+            var latDDPattern = @"^(\+|-)?(?:90(?:(?:\.0{1,20})?)|(?:[0-9]|[1-8][0-9])(?:(?:\.[0-9]{1,20})?))$";
+
+            ///Get rid of any white spaces
+            dms = dms.Trim();
+
+            double coordinate = Double.NaN;
+
+            ///Test if the Lat is DD format and return as double if valid
+            if (Regex.Match(dms, latDDPattern).Success)
+            {
+                return Double.Parse(dms);
+            }
+
+            ///Else test if the Lat is DMS format and convert to double DD and return if valid
+            else if (Regex.Match(dms, latDMSPattern).Success && Regex.Match(dms, latDMSPattern).Value.Length == dms.Length)
+            {
+                bool sw = dms.ToLower().EndsWith("s");
+                int f = sw ? -1 : 1;
+                var bits = Regex.Matches(dms, @"[\d.]+", RegexOptions.IgnoreCase);
+                coordinate = 0;
+                double result;
+                for (int i = 0; i < bits.Count; i++)
+                {
+                    if (Double.TryParse(bits[i].ToString(), out result))
+                    {
+                        coordinate += result / f;
+                        f *= 60;
+                    }
+                }
+                return coordinate;
+            }
+
+            ///If DD or DMS format is invalid, return NaN
+            else
+            {
+                return coordinate;
+            }
+        }
+
     }
 }
