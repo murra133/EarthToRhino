fd5e129f5f586d03eef5dc3a2ce7602516237265
enmerk4r <p.i.g.a.c.h@gmail.com>
2024-10-26 14:55:44 -0400

    Working authenticated API calls

---
+++ EarthToRhino/TileHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EarthToRhino
+{
+    public class TileHandler
+    {
+        public TileHandler()
+        {
+
+        }
+
+        public void DownloadTiles()
+        {
+
+        }
+
+        public void GetRoot()
+        {
+            string rootObject = WebAPI.Get(RoutesController.Root, true);
+        }
+    }
+}
