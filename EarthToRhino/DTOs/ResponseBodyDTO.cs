d19ca1668e9d8711cba043851640b0ae6c1e3539
Jeroen Janssen <58253502+JeroenJanssen-TT@users.noreply.github.com>
2024-10-26 20:33:38 -0400

    Error catching
    API connection error call

---
+++ EarthToRhino/DTOs/ResponseBodyDTO.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace EarthToRhino
+{
+    public class ResponseBodyDTO
+    {
+        [JsonProperty("error")]
+        public ErrorDTO Error { get; set; }
+        public class ErrorDTO
+        {
+            [JsonProperty("message")]
+            public string Message { get; set; }
+        }
+    }
+}
