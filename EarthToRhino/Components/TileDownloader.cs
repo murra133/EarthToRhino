6a0e66b91fb89ed45fc2a90dcb881cc2fec162be
enmerk4r <p.i.g.a.c.h@gmail.com>
2024-10-26 22:29:10 -0400

    Working Recursion

--- EarthToRhino/Components/TileDownloader.cs
+++ EarthToRhino/Components/TileDownloader.cs
@@ -27,12 +27,11 @@ namespace EarthToRhino.Components
         /// </summary>
         protected override void RegisterInputParams(GH_InputParamManager pManager)
         {
-            pManager.AddIntegerParameter("Level of Detail", "D", "The level of detail", GH_ParamAccess.item);
+            pManager.AddIntegerParameter("Level of Detail", "D", "The level of detail (expressed as recursion depth)", GH_ParamAccess.item);
             pManager.AddRectangleParameter("Boundary", "B", "The boundary of the area to download", GH_ParamAccess.item);
             pManager.AddTextParameter("Temp Folder", "F", "The temporary folder to store the tiles", GH_ParamAccess.item);
             pManager.AddTextParameter("API Key", "K", "The API key", GH_ParamAccess.item);
 
-            pManager[0].Optional = true;
             pManager[1].Optional = true;
         }
 
@@ -64,7 +63,7 @@ namespace EarthToRhino.Components
             string tempFolder = "";
             string apiKey = "";
 
-            DA.GetData(0, ref levelOfDetail);
+            
             DA.GetData(1, ref boundary);
             DA.GetData(2, ref tempFolder);
             DA.GetData(3, ref apiKey);
@@ -88,30 +87,24 @@ namespace EarthToRhino.Components
 
             TileHandler tileHandler = new TileHandler();
 
+            if (DA.GetData(0, ref levelOfDetail))
+            {
+                tileHandler.SetRecursionDepth(levelOfDetail);
+            }
+            else
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, "Level of Detail is missing");
+                return;
+            }
+
+
             TileClusterDTO rootCluster = tileHandler.GetTileCluster(RoutesController.Root);
 
-            List<BoundingVolumeDTO> bboxes = new List<BoundingVolumeDTO>();
+            tileHandler.UnpackTileRecursive(rootCluster.Root, 0);
 
-            foreach (ChildDTO child in rootCluster.Root.Children)
-            {
-                foreach (ChildDTO grandChild in child.Children)
-                {
-                    TileClusterDTO firstLayer = tileHandler.GetTileCluster(grandChild.Content.Uri);
+            tileHandler.DownloadAllChildren();
 
-                    foreach (ChildDTO firstLayerChild in firstLayer.Root.Children)
-                    {
-                        foreach (ChildDTO firstLayerGrandchild in firstLayerChild.Children)
-                        {
-                            if (tileHandler.TrySaveChild(firstLayerGrandchild))
-                            {
-                                bboxes.Add(firstLayerGrandchild.BoundingVolume);
-                            }
-                            
-                        }
-                        
-                    }
-                }
-            }
+            List<BoundingVolumeDTO> bboxes = tileHandler.GetAllBoundingVolumes();
 
             GH_Structure<GH_Number> dataTree = new GH_Structure<GH_Number>();
 
--- EarthToRhino/TileHandler.cs
+++ EarthToRhino/TileHandler.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using Rhino.Geometry;
 using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
@@ -12,9 +13,30 @@ namespace EarthToRhino
 {
     public class TileHandler
     {
+        public int MaxRecursionDepth { get; private set; }
+
+        public List<ChildDTO> ChildrenToDownload { get; private set; }
         public TileHandler()
         {
+            ChildrenToDownload = new List<ChildDTO>();
+        }
 
+        public void SetRecursionDepth(int depth)
+        {
+            MaxRecursionDepth = depth;
+        }
+
+        public void DownloadAllChildren()
+        {
+            foreach (ChildDTO child in this.ChildrenToDownload)
+            {
+                TrySaveChild(child);
+            }
+        }
+
+        public List<BoundingVolumeDTO> GetAllBoundingVolumes()
+        {
+            return this.ChildrenToDownload.Select(c => c.BoundingVolume).ToList();
         }
 
         public TileClusterDTO GetTileCluster(string partialUri)
@@ -57,6 +79,87 @@ namespace EarthToRhino
             return WebAPI.DownloadGLB(uri, fullpath);
         }
 
-        
+        public void UnpackTileRecursive(ChildDTO child, int recursionDepth)
+        {
+            // If max recursion depth is reached, add the child 
+            // to the download list and return
+            if (recursionDepth >= this.MaxRecursionDepth)
+            {
+                this.ChildrenToDownload.Add(child);
+                return;
+            }
+
+            NextStepCondition nextStep = EvaluateNextStep(child);
+
+            switch (nextStep)
+            {
+                case NextStepCondition.HasUnpackableChildren:
+                    foreach (ChildDTO c in child.Children)
+                    {
+                        if (IsViableTile(c))
+                        {
+                            UnpackTileRecursive(c, recursionDepth + 1);
+                        }
+                    }
+                    break;
+                case NextStepCondition.NextIsJSON:
+                    ChildDTO jsonChild = child.Children.First();
+
+                    TileClusterDTO cluster = this.GetTileCluster(jsonChild.Content.Uri);
+
+                    foreach (ChildDTO c in cluster.Root.Children)
+                    {
+                        if (IsViableTile(c))
+                        {
+                            UnpackTileRecursive(c, recursionDepth + 1);
+                        }
+                    }
+                    break;
+                case NextStepCondition.IsLeaf:
+                    this.ChildrenToDownload.Add(child);
+                    return;
+            }
+        }
+
+        public bool IsViableTile(ChildDTO child)
+        {
+            // This is for Nico to implement. The idea here is to evaluate
+            // Whether or not child is one of the following:
+            // 1. Contains the child tile
+            // 2. Is contained by the child
+            // 3. Intersects the child
+
+            return true;
+        }
+
+
+        public NextStepCondition EvaluateNextStep(ChildDTO child)
+        {
+            if (child.Children == null || child.Children.Count == 0)
+            {
+                return NextStepCondition.IsLeaf;
+            }
+
+            if (child.Children.Any(c => c.Content != null && !string.IsNullOrEmpty(c.Content.Uri) && c.Content.Uri.Contains(".json")))
+            {
+                return NextStepCondition.NextIsJSON;
+            }
+
+            if (child.Children.Any(c => (c.Content != null && !string.IsNullOrEmpty(c.Content.Uri) && c.Content.Uri.EndsWith(".glb")) || (c.Children != null && c.Children.Count > 0)))
+            {
+                return NextStepCondition.HasUnpackableChildren;
+            }
+
+            return NextStepCondition.IsLeaf;
+        }
+
+        public enum NextStepCondition
+        {
+            HasUnpackableChildren = 0,
+            NextIsJSON = 1,
+            IsLeaf = 2
+        }
+
+
     }
 }
